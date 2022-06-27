599093a1a6128c34002447708804d29f27fcd63f
Muhammad Zeeshan <mzeeshan.bscs19seecs@seecs.edu.pk>
2022-06-28 01:35:58 +0500

    initial Commit

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "./intro.scss";
+export default function intro() {
+  return <div className="intro">intro</div>;
+}
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,5 @@
+import "./topbar.scss";
+
+export default function Topbar() {
+  return <div className="topbar"> hello how are you </div>;
+}
